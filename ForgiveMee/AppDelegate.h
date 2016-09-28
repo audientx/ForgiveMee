6bac5d8858374d56c89514f5e39a05e86cd7ca4f
audientx <audientx@163.com>
2016-09-28 15:31:11 +0800

    add

---
+++ ForgiveMee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ForgiveMee
+//
+//  Created by Audient Xie on 2016/9/28.
+//  Copyright © 2016年 xjk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ForgiveMee/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ForgiveMee
+//
+//  Created by Audient Xie on 2016/9/28.
+//  Copyright © 2016年 xjk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
